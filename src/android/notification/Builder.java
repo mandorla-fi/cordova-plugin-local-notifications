9973f28a48f1b0cf1a5163968ce799af0032a243
Tommi Ketola <tommi.ketola@teleste.com>
2017-06-06 20:15:07 +0300

    Use BigTextStyle.

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -128,7 +128,8 @@ public class Builder {
                 .setTicker(options.getText())
                 .setAutoCancel(options.isAutoClear())
                 .setOngoing(options.isOngoing())
-                .setColor(options.getColor());
+                .setColor(options.getColor())
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(options.getText()));
 
         if (ledColor != 0) {
             builder.setLights(ledColor, options.getLedOnTime(), options.getLedOffTime());
